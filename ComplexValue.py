aeb76a5df28bfcf59f0189d5c2ec89dc2049c6e9
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2017-05-15 01:57:50 -0700

    Moved standard units out of run into helper classes

--- ComplexValue.py
+++ ComplexValue.py
@@ -37,6 +37,48 @@ class ComplexValue(object):
 			return "invalid"
 		return str(self.value) + " " + self.unit.to_string()
 
+si_library = {}
+derived_library = {}
+
+si_library["unitless"] = cu. unitless
+si_library["kilogram"] = cu.kilogram
+si_library["second"] = cu.second
+si_library["ampere"] = cu.ampere
+si_library["kelvin"] = cu.kelvin
+si_library["candela"] = cu.candela
+derived_library["hertz"] = cu. hertz
+derived_library["meterpersecond"] = cu.meterpersecond
+derived_library["meterpersecondsquared"] = cu.meterpersecondsquared
+derived_library["kilogrammeter"] = cu.kilogrammeter
+derived_library["kelvin"] = cu.kelvin
+derived_library["metersquared"] = cu.metersquared
+derived_library["metercubed"] = cu. metercubed
+derived_library["newton"] = cu.newton
+derived_library["joule"] = cu.joule
+derived_library["watt"] = cu.watt
+derived_library["pascal"] = cu.pascal
+derived_library["coulomb"] = cu.coulomb
+derived_library["volt"] = cu.volt
+derived_library["farad"] = cu.farad
+derived_library["ohm"] = cu. ohm
+derived_library["siemen"] = cu.siemen
+derived_library["weber"] = cu.weber
+derived_library["tesla"] = cu.tesla
+derived_library["henry"] = cu.henry
+
+standard_library = si_library.copy()
+standard_library.update(derived_library)
+
+def unit_lookup(library, unit):
+	for comparison_unit in library:
+		if cu.equals(unit, library[comparison_unit]):
+			return comparison_unit
+	return unit.to_string()
+
+def show(complex_value):
+	unitname = unit_lookup(standard_library, complex_value.get_unit())
+	print str(complex_value.get_value()) + " " + unitname
+
 # Negate a complex value
 def negate(cv):
 	return ComplexValue(-cv.get_value(), cv.get_unit())
--- Run.py
+++ Run.py
@@ -1,34 +1,7 @@
 import ComplexUnit as cu
 import ComplexValue as cv
 
-unitless = cu.ComplexUnit({})
-kilogram = cu.ComplexUnit({"kg":1})
-meter = cu.ComplexUnit({"m":1})
-second = cu.ComplexUnit({"s":1})
-ampere = cu.ComplexUnit({"A":1})
-kelvin = cu.ComplexUnit({"K":1})
-candela = cu.ComplexUnit({"cd":1})
-si_library = [unitless, kilogra, meter, second, ampere, kelvin, candela]
-
-hertz = cu.reciprocal(second)
-meterpersecond = cu.divide(meter, second)
-meterpersecondsquared = cu.divide(meterpersecond, second)
-kilogrammeter = cu.multiply(meter, kilogram)
-metersquared = cu.multiply(meter, meter)
-metercubed = cu.multiply(metersquared, meter)
-newton = cu.multiply(meterpersecondsquared, kilogram)
-joule = cu.multiply(newton, meter)
-watt = cu.divide(joule, second)
-pascal = cu.divide(newton, metersquared)
-coulomb = cu.multiply(second, ampere)
-volt = cu.divide(watt, ampere)
-farad = cu.divide(coulomb, volt)
-ohm = cu.divide(volt, ampere)
-siemen = cu.reciprocal(ohm)
-weber = cu.divide(joule, ampere)
-tesla = cu.divide(weber, metersquared)
-henry = cu.divide(weber, ampere)
-derived_library = [hertz, meterpersecond, meterpersecondsquared, kilogrammeter, metersquared, metercubed, newton, joule, watt, pascal, coulomb, volt, farad, ohm, siemen, weber, tesla, henry]
-
-standard_library = si_library + derived_library
-
+a = cv.ComplexValue(20, cu.joule)
+b = cv.ComplexValue(3, cu.second)
+c = cv.divide(a, b)
+cv.show(c)
