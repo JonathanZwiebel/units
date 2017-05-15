3cdfc0a2e0546ff25a0359c1707302b54ecbf7b5
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2017-05-15 02:33:38 -0700

    Fixed derived unit names

--- Run.py
+++ Run.py
@@ -1,10 +1,12 @@
 import ComplexUnit as cu
 import ComplexValue as cv
 
-a = cv.ComplexValue(20.0, cu.watt)
-b = cv.ComplexValue(14.0, cu.watt)
-c = cv.ComplexValue(7.0, cu.watt)
-d = cv.ComplexValue(-845, cu.watt)
+t = cv.ComplexValue(6, cu.second)
+v0 = cv.ComplexValue(20, cu.meterspersecond)
+x0 = cv.ComplexValue(15, cu.meter)
 
-f = cv.add(a, b, c, d)
-cv.show(f)
+g = cv.ComplexValue(-9.8, cu.meterspersecondsquared)
+half = cv.ComplexValue(0.5, cu.unitless)
+
+distance = cv.add(cv.multiply(t, t, g, half), cv.multiply(v0, t), x0)
+cv.show(distance)
